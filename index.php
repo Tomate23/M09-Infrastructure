8136f0f69ee17ba6a68a8f57e80976263f901ce0
Tomate23 <a19osmvalval@inspedralbes.cat>
2021-02-17 12:16:56 +0100

    icon

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <html lang="en">
 
 <head>
+
+    <link rel="icon" type="image/x-icon" href="./img/chili-pepper.png" />
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Incidencias</title>
